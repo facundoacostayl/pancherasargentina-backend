25da7f98d037c86b193e6c7b91953c750a4781b1
facundoacostayl <facundoacostayl@outlook.com>
2024-01-07 09:00:12 -0300

    update group's id and project's name

---
+++ src/test/java/com/pancherasargentina/server/pancherasargentinaserver/PancherasArgentinaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pancherasargentina.server.pancherasargentinaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PancherasArgentinaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
