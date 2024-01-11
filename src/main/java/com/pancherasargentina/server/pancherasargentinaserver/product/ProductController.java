2b24ca9148501742aa0c94c16897812279e2f5ce
facundoacostayl <facundoacostayl@outlook.com>
2024-01-10 22:40:48 -0300

    update main class enabling cors

--- src/main/java/com/pancherasargentina/server/pancherasargentinaserver/product/ProductController.java
+++ src/main/java/com/pancherasargentina/server/pancherasargentinaserver/product/ProductController.java
@@ -1,6 +1,7 @@
 package com.pancherasargentina.server.pancherasargentinaserver.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/product")
+@RequestMapping("/api/v1/product/")
 public class ProductController {
 
     private final ProductService productService;
--- src/main/java/com/pancherasargentina/server/pancherasargentinaserver/shipping/ShippingController.java
+++ src/main/java/com/pancherasargentina/server/pancherasargentinaserver/shipping/ShippingController.java
@@ -1,6 +1,7 @@
 package com.pancherasargentina.server.pancherasargentinaserver.shipping;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import java.time.Month;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/shipping")
+@RequestMapping("/api/v1/shipping/")
 public class ShippingController {
 
     private final ShippingService shippingService;
