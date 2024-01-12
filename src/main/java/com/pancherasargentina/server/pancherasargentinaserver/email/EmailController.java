0a429a4137db93530470fc33ec3daadcacd4c67c
facundoacostayl <facundoacostayl@outlook.com>
2024-01-12 19:36:56 -0300

    add emailSender

--- src/main/java/com/pancherasargentina/server/pancherasargentinaserver/email/EmailController.java
+++ src/main/java/com/pancherasargentina/server/pancherasargentinaserver/email/EmailController.java
@@ -1,7 +1,28 @@
 package com.pancherasargentina.server.pancherasargentinaserver.email;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class EmailController {
+@RequestMapping("/api/v1/email")
+public class EmailController
+{
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @PostMapping
+    public void sendSimpleMessage(
+            String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("facundoacostayl@gmail.com");
+        message.setTo("kittyfacu21@gmail.com");
+        message.setSubject("Compra Pagina");
+        message.setText("Quiero realizar una consulta acerca de las pancheras.");
+        emailSender.send(message);
+    }
 }
